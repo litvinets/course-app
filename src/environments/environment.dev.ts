86bb52db1d9707cb983651e643255c60226944d2
Oleksandra Lytvynets <alitvinets7@gmail.com>
2023-05-11 17:49:46 +0300

    styles: update global app styles (#1)

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,14 +1,14 @@
 export const environment = {
     production: false,
     name: 'dev',
-  firebase: {
-      config: {
-          projectId: 'courseapp-c7a46',
-          appId: '1:280416194259:web:3f0e24c01eb03e9c8b0e26',
-          storageBucket: 'courseapp-c7a46.appspot.com',
-          apiKey: 'AIzaSyAGgVfeL4OcxlCeoXZHgTBxM2xdRkQbFb4',
-          authDomain: 'courseapp-c7a46.firebaseapp.com',
-          messagingSenderId: '280416194259',
-      }
-  }
+    firebase: {
+        config: {
+            projectId: 'courseapp-c7a46',
+            appId: '1:280416194259:web:3f0e24c01eb03e9c8b0e26',
+            storageBucket: 'courseapp-c7a46.appspot.com',
+            apiKey: 'AIzaSyAGgVfeL4OcxlCeoXZHgTBxM2xdRkQbFb4',
+            authDomain: 'courseapp-c7a46.firebaseapp.com',
+            messagingSenderId: '280416194259',
+        }
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,16 @@
 export const environment = {
     production: false,
     name: 'default',
-  firebase: {
-      config: {
-          projectId: 'courseapp-c7a46',
-          appId: '1:280416194259:web:3f0e24c01eb03e9c8b0e26',
-          storageBucket: 'courseapp-c7a46.appspot.com',
-          apiKey: 'AIzaSyAGgVfeL4OcxlCeoXZHgTBxM2xdRkQbFb4',
-          authDomain: 'courseapp-c7a46.firebaseapp.com',
-          messagingSenderId: '280416194259',
-      }
-  }
+    firebase: {
+        config: {
+            projectId: 'courseapp-c7a46',
+            appId: '1:280416194259:web:3f0e24c01eb03e9c8b0e26',
+            storageBucket: 'courseapp-c7a46.appspot.com',
+            apiKey: 'AIzaSyAGgVfeL4OcxlCeoXZHgTBxM2xdRkQbFb4',
+            authDomain: 'courseapp-c7a46.firebaseapp.com',
+            messagingSenderId: '280416194259',
+        }
+    }
 };
 
 /*
