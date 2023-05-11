4c339f111259cb5f29fe49537af99ec72785b716
Oleksandra Lytvynets <alitvinets7@gmail.com>
2023-05-11 17:49:05 +0300

    styles: update global app styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../../src/environments/environment';
+import { environment } from '../environments/environment';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import {AngularFireModule} from "@angular/fire/compat";
-import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
-import {AngularFireStorageModule} from "@angular/fire/compat/storage";
-import {getFirestore, provideFirestore} from "@angular/fire/firestore";
-import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 
 @NgModule({
     declarations: [
@@ -24,9 +21,6 @@ import {AngularFireAuthModule} from "@angular/fire/compat/auth";
         AngularFirestoreModule,
         AngularFireAuthModule,
         AngularFireStorageModule,
-        BrowserAnimationsModule,
-        provideFirebaseApp(() => initializeApp(environment.firebase.config)),
-        provideFirestore(() => getFirestore()),
     ],
     providers: [],
     bootstrap: [AppComponent]
