7322c87c6c20d05d5cc656bccf3f88d26aa5de6f
Oleksandra Lytvynets <alitvinets7@gmail.com>
2023-06-26 14:40:44 +0300

    cabinet/ orders

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ShellRoutingModule } from './shell-routing.module';
-import { ShellComponent } from '../shell/shell.component';
-import {FlexModule} from '@angular/flex-layout';
-import { NotFoundPageComponent } from '@app/shell/not-found-page/not-found-page.component';
-import {LoaderModule} from "@app/shared/components/loader/loader.module";
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ShellRoutingModule } from "./shell-routing.module";
+import { ShellComponent } from "../shell/shell.component";
+import { FlexModule } from "@angular/flex-layout";
+import { NotFoundPageComponent } from "@app/shell/not-found-page/not-found-page.component";
+import { LoaderModule } from "@app/shared/components/loader/loader.module";
 
 @NgModule({
   declarations: [ShellComponent, NotFoundPageComponent],
